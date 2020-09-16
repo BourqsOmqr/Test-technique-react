3c6346ea64f9e49b9924cd4054756ba538aeb6d3
Bao CHAU <bao.chau@netdevices.fr>
2020-09-16 20:14:02 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Layout from './layout';
 import Home from './pages/Home';
+import Todos from './pages/Todos';
+import NotFound from './pages/NotFound';
 
 const App = (): JSX.Element => (
-  <Layout>
-    <Home />
-  </Layout>
+  <Router>
+    <Layout>
+      <Switch>
+        <Route exact path={['/', '/home']} component={Home} />
+        <Route path='/todos' component={Todos} />
+        <Route path='*' component={NotFound} />
+      </Switch>
+    </Layout>
+  </Router>
 );
 
 export default App;
