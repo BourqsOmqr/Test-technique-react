292844ddb4a2a6cf787b19482d7c4e46bee91f83
Bao CHAU <bao.chau@netdevices.fr>
2020-09-16 20:11:33 +0200

    add menu navigation

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,5 +1,6 @@
 import './index.scss';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 type Props = {
   children: React.ReactElement,
@@ -9,6 +10,10 @@ const Layout = ({ children }: Props): JSX.Element => (
   <div className="Layout">
     <header className="Layout__header">
       <h1>Test technique React.js</h1>
+      <nav className='header__menu'>
+        <Link to='/home'>Home</Link>
+        <Link to='/todos'>Todos</Link>
+      </nav>
     </header>
     <main className="Layout__main">
       {children}
