69719d37937f2ef6fcd42d3e8d5caf62b7f5d38d
Bao CHAU <bao.chau@netdevices.fr>
2020-09-16 20:13:07 +0200

    add fake data for todo list

---
+++ src/mock-data.tsx
@@ -0,0 +1,5 @@
+export const todoList  = [
+  { id: 1, title: 'aghuyg', content: 'bafouazhg'},
+  { id: 2, title: 'gzrhteyj', content: 'bafouazhg'},
+  { id: 3, title: 'aghagzrhzthuyg', content: 'bafouazhg'}
+]
